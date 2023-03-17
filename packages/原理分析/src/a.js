05b37c177fc73934db07dc2b1f0b2a938dc81dfc
benyueer <347201906@qq.com>
2023-03-17 17:13:37 +0800

    feat: lesson

--- packages/原理分析/src/a.js
+++ packages/原理分析/src/a.js
@@ -1,1 +1,11 @@
+const loadB = async () => {
+  import('./b.js')
+}
+const loadC = async () => {
+  import('./c.js')
+}
+
 export const name = 'a'
+
+document.querySelector('#btn1').addEventListener('click',loadB)
+document.querySelector('#btn2').addEventListener('click',loadC)
---
+++ packages/原理分析/src/c.js
@@ -0,0 +1,3 @@
+export const data = {
+  text: 'this is c.js'
+}
